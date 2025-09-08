0ff847576d8f44f34104de97a442279ae4e3b908
acacore <mensart81@outlook.com>
2025-09-08 12:43:08 +0000

    Code refined

--- 4-concat.js
+++ 4-concat.js
@@ -5,8 +5,10 @@ for (let i of processArgv) {
   count++;
 }
 
-if (count === 2) {
-  console.log("No argument");
-} else {
+if (count == 2) {
+  console.log("undefined is undefined");
+} else if (count == 3) {
+    console.log(`${processArgv[2]} is undefined`)
+}else{
   console.log(processArgv.slice(2, 4).join(" is "));
 }
